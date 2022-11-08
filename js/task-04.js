2c9b4f0ca5665e3e69885ae3dc190f36b9f2d04f
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-08 13:58:58 +0200

    task 5

--- js/task-04.js
+++ js/task-04.js
@@ -5,7 +5,18 @@ const actions = document.querySelectorAll('#counter');
 console.log(actions);
 console.log(actions.firstElementChild);
 
-const addValueBtn = document.querySelector('dataset.action');
+const addValueBtn = document.querySelector('[data-action="increment"]');
 // console.log(addValueBtn);
 
 addEventListener('click', () => {});
+
+// function addButtonClickHandler() {
+//   console.log('Клік');
+// }
+
+// function handleAddButtonClick() {
+//   console.log('Клік');
+// }
+// function onAddButtonClick() {
+//   console.log('Клік');
+// }
--- js/task-05.js
+++ js/task-05.js
@@ -3,10 +3,39 @@
 //  його поточне значення в span#name - output.
 //  Якщо інпут порожній, у спані повинен відображатися
 //  рядок "Anonymous".
-const inputEl = document.querySelector('#name-input');
-console.log(inputEl);
-const outputText = document.querySelector('#name-output');
+const input = document.querySelector('#name-input');
+console.log(input);
+const output = document.querySelector('#name-output');
 
-if (inputEl.value !== '') {
-  return (outputText = inputEl.value);
+const refs = {
+  input: document.querySelector('#name-input');
+  output: document.querySelector('#name-output');
 }
+// refs.input.addEventListener("focus", onInputFocus);
+// refs.input.addEventListener("blur", onInputBlur);
+// refs.input.addEventListener("change", onInputChange);
+
+refs.input.addEventListener("input", onInputChange);
+
+function onInputFocus() {
+  console.log("інпут отримав фокус");
+}
+function onInputBlur() {
+  console.log("інпут втратив фокус");
+}
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
+  refs.output.textContent = event.currentTarget.value;
+}
+// function onLicenseChenge(event) {
+//   console.log(event);
+// }
+
+
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   console.log(event);
+//   console.log(event.currentTarget);
+//   console.log('це сабміт форми без перезагрузки сторінки');
+// }

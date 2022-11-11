5478fd252cac77806b43adf89d2a72a22c5dee7f
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-11-11 21:17:02 +0200

    fixed 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,18 @@ const inputValue = document.querySelector('#validation-input');
 function onInputBlur(event) {
   const validLength = event.currentTarget.value.trim().length;
 
-  //   console.log(validLength);
-  //   console.log(typeof validLength);
-  //   console.log(inputValue.dataset.length);
+  // console.log(validLength);
+  // console.log(typeof validLength);
+  // console.log(inputValue.dataset.length);
 
   if (validLength == inputValue.dataset.length) {
-    return inputValue.classList.add('valid');
+    inputValue.classList.add('valid');
+    inputValue.classList.remove('invalid');
+  } else {
+    inputValue.classList.add('invalid');
+    inputValue.classList.remove('valid');
   }
-  inputValue.classList.add('invalid');
 }
 
 inputValue.addEventListener('blur', onInputBlur);
+console.log(inputValue);
